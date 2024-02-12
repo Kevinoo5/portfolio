f4a0adc0aaf45af7c2b162d18ac8a78fd835980c
Kevinoo5 <kevinnoormets@gmail.com>
2024-02-13 00:19:58 +0200

    Removed placeholder border and created background gradient for list items

--- src/common/AnimatedListItem.jsx
+++ src/common/AnimatedListItem.jsx
@@ -1,27 +1,32 @@
 import { useInView } from 'react-intersection-observer';
 
 const AnimatedListItem = ({ project }) => {
-    const [ref, inView] = useInView({
-      triggerOnce: true,
-      threshold: 0,
-    });
-  
-    return (
-<li ref={ref} className={`border border-red-600 mx-24 flex ${inView ? 'animate-fade-left' : 'opacity-0'}`}>
-  <div className="my-6 mx-8">
-    <img
-      className="rounded-3xl min-w-[300px] min-h-[250px] w-[300px] h-[250px]"
-      src={project.imageUrl}
-      alt={project.title}
-    />
-  </div>
-  <div className="my-4 mx-12 flex flex-col justify-center">
-    <h2 className="text-white mb-8 text-2xl">{project.title}</h2>
-    <p className="text-white">{project.description}</p>
-  </div>
-</li>
+  const [ref, inView] = useInView({
+    triggerOnce: true,
+    threshold: 0,
+  });
 
-    );
-  };
+  return (
+    <li
+    ref={ref}
+    className={`p-1 bg-gradient-to-b from-blue-900 to-blue-950 rounded-xl mb-4 mx-24 flex ${
+      inView ? "animate-fade-left" : "opacity-0"
+    }`}
+  >
+    <div className="my-6 mx-8">
+      <img
+        className="rounded-3xl min-w-[300px] min-h-[250px] w-[300px] h-[250px]"
+        src={project.imageUrl}
+        alt={project.title}
+      />
+    </div>
+    <div className="my-4 mx-12 flex flex-col justify-center">
+      <h2 className="text-white mb-8 text-2xl">{project.title}</h2>
+      <p className="text-white">{project.description}</p>
+    </div>
+  </li>
+  
+  );
+};
 
 export default AnimatedListItem;
