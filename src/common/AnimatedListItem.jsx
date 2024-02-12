0eb1fc5b95f3dbb52a14d9c6b6369ed3422e2e49
Kevinoo5 <kevinnoormets@gmail.com>
2024-02-12 23:28:48 +0200

    Changed animated list item design and added first item to work list. Made banner be full viewport height.

--- src/common/AnimatedListItem.jsx
+++ src/common/AnimatedListItem.jsx
@@ -7,16 +7,20 @@ const AnimatedListItem = ({ project }) => {
     });
   
     return (
-      <li ref={ref} className={`flex justify-start border border-red-600 mx-24 ${inView ? 'animate-fade-left' : 'opacity-0'}`}>
-        <div className="my-4 mx-8">
-          <img
-            className="rounded-3xl"
-            src={project.imageUrl}
-            alt={project.title}
-          />
-          <p>{project.description}</p>
-        </div>
-      </li>
+<li ref={ref} className={`border border-red-600 mx-24 flex ${inView ? 'animate-fade-left' : 'opacity-0'}`}>
+  <div className="my-6 mx-8">
+    <img
+      className="rounded-3xl min-w-[300px] min-h-[300px] w-[300px] h-[300px]"
+      src={project.imageUrl}
+      alt={project.title}
+    />
+  </div>
+  <div className="my-4 mx-12 flex flex-col justify-center">
+    <h2 className="text-white mb-8 text-2xl">{project.title}</h2>
+    <p className="text-white">{project.description}</p>
+  </div>
+</li>
+
     );
   };
 
--- src/data/workList.jsx
+++ src/data/workList.jsx
@@ -1,15 +1,15 @@
 const projects = [
   {
     id: 1,
-    title: "Project 1",
-    imageUrl: "https://via.placeholder.com/300",
-    description: "Description of Project 1",
+    title: "Aasta Tegija 2024 Competition",
+    imageUrl: "voco.png",
+    description: "This was project was made for the Aasta Tegija 2024 competition. The competition was held on the 17-18th of January in 2024 at Tartu Vocational College, it was done in teams of two to three students who had to recreate a randomly chosen Figma prototype made by other students with code in about 12 hours. The prototype was made for the Baltic Games for Vocational Schools. The website was made using plain HTML and CSS"
   },
   {
     id: 2,
     title: "Project 2",
     imageUrl: "https://via.placeholder.com/300",
-    description: "Description of Project 2",
+    description: "Description of Project 2"
   },
 ];
 
