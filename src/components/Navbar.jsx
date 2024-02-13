0eb4bd272515a7bf577ccee88c2d3eaf31ef7f2e
Kevinoo5 <kevinnoormets@gmail.com>
2024-02-13 23:45:26 +0200

    Made navbar fixed and blur its background

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import NavbarItem from "../common/NavbarItem";
 
 const Navbar = () => {
   return (
-    <div className="flex items-center h-24 mx-auto px-12 text-white">
+    <div className="flex fixed w-full z-10 backdrop-blur-xl items-center h-24 mx-auto px-12 text-white">
       <div className="flex-1">
         <a href="/" className="text-3xl font-bold text-[#00df9a]">
           KEVIN NOORMETS
