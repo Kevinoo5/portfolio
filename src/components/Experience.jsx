ad63932136524e4d97036595d29c0a76689f355e
Kevinoo5 <kevinnoormets@gmail.com>
2024-02-14 17:29:54 +0200

    Refactored Experience component and updated work experience details

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,39 +1,56 @@
 import React from "react";
 import {
-    Accordion,
-    AccordionHeader,
-    AccordionBody,
-  } from "@material-tailwind/react";
+  Accordion,
+  AccordionHeader,
+  AccordionBody,
+} from "@material-tailwind/react";
 import AnimatedTitle from "../common/AnimatedTitle";
-   
-  const Experience = () => {
-    const [open, setOpen] = React.useState(1);
-   
-    const handleOpen = (value) => setOpen(open === value ? 0 : value);
-   
-    return (
-      <div id="experience" className="mx-2 lg:mx-64">
-        <AnimatedTitle title="Experience" />
-        <Accordion open={open === 1} className="mb-2 rounded-lg">
+
+const Experience = () => {
+  const [open, setOpen] = React.useState(1);
+
+  const handleOpen = (value) => setOpen(open === value ? 0 : value);
+
+  return (
+    <div id="experience" className="mx-2 lg:mx-64">
+      <AnimatedTitle title="Work Experience" />
+      <Accordion open={open === 1} className="mb-2 rounded-lg">
         <AccordionHeader
-      onClick={() => handleOpen(1)}
-      className={"flex justify-evenly py-8 rounded-xl transition-colors text-white bg-gradient-to-b from-purple-950 to-[#3f2350]"}
-    >
-      <div className="pl-6">
-        Frontend Developer on <strong>Volugram</strong>
-      </div>
-      <div className="">January 2024 - Present</div>
-    </AccordionHeader>
-          <AccordionBody className="mt-2 pl-6 bg-[#4e3f8d] text-white">
-            <p>
-              <strong>Frontend Developer</strong>
-              <br />
-              <span>January 2024 - Present</span>
-            </p>
-          </AccordionBody>
-        </Accordion>
-      </div>
-    );
-}
+          onClick={() => handleOpen(1)}
+          className={
+            "flex justify-evenly py-8 rounded-xl transition-colors text-white bg-gradient-to-b from-purple-950 to-[#3f2350]"
+          }
+        >
+          <div className="pl-28">
+            Frontend Developer on <strong>Volugram</strong>
+          </div>
+          <div>January 2024 - Present</div>
+        </AccordionHeader>
+        <AccordionBody className="mt-2 pl-6 rounded-md bg-[#4e3f8d] text-white">
+        <img src="https://volugram.eu/wp-content/uploads/2024/02/VOLUGRAM-Logo-White.png" alt="Volugram" className="w-1/4" />
+
+          <p className="w-full">
+            Developing the frontend of the Volugram volunteer recognition
+            platform using Vue.js and Tailwind CSS. I joined the team late into
+            the project and was able to work on the final stages of the
+            development process along with adding and changing certain elements
+            according to client feedback.
+          </p>
+
+          <ul className="flex flex-row mt-2 mb-1">
+            <li className="mr-2 my-2 rounded-full p-3 bg-[#6e53da]">
+              JavaScript
+            </li>
+            <li className="mr-2 my-2 rounded-full p-3 bg-[#6e53da]">Vue.js</li>
+            <li className="mr-2 my-2 rounded-full p-3 bg-[#6e53da]">
+              Tailwind CSS
+            </li>
+          </ul>
+
+        </AccordionBody>
+      </Accordion>
+    </div>
+  );
+};
 
 export default Experience;
