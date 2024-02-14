6be7db3de61921f79b04cbca19a2929df3cddd9b
Kevinoo5 <kevinnoormets@gmail.com>
2024-02-14 16:55:38 +0200

    Update footer styling and text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
-        <footer className="flex justify-center items-center bottom-0 left-0 mt-24 py-4 bg-slate-600">
-            <p className="text-white">This is the footer.</p>
+        <footer className="flex justify-center items-center bottom-0 left-0 mt-24 py-4 bg-transparent">
+            <p className="text-white text-sm">Made by Kevin Noormets</p>
         </footer>
     );
 };
