551d2a7493586a8831f212b9f8b5fee60e80eb53
Kevinoo5 <kevinnoormets@gmail.com>
2024-02-16 18:08:44 +0200

    Add email functionality and toast notifications to Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,28 @@
+import emailjs from '@emailjs/browser';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { useRef } from 'react';
+
 const Footer = () => {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm(import.meta.env.VITE_MAIL_SERVICE, VITE_MAIL_TEMPLATE, form.current, {
+            publicKey: import.meta.env.VITE_MAIL_PUBLICKEY,
+        })
+            .then((result) => {
+                console.log("Email sent successfully!");
+                form.current.reset();
+                toast.success('Email sent successfully!');
+            })
+            .catch((error) => {
+                console.error("Error sending email: ", error);
+                toast.error('Error sending email');
+            });
+    }
+
   return (
     <footer className="flex text-white justify-around bottom-0 left-0 pt-24 pb-16 bg-slate-800">
       <div className="flex flex-col pl-4 pr-4 md:px-12">
@@ -19,21 +43,27 @@ const Footer = () => {
           </a>
         </div>
       </div>
-      <form className="flex flex-col gap-4 w-1/2">
+      <form ref={form} onSubmit={sendEmail} className="flex flex-col gap-4 w-1/2">
         <input
           type="text"
+          name="from_name"
           placeholder="Full Name"
           className="bg-slate-700 text-white w-11/12 md:w-4/5 p-2 h-12 rounded-md"
+          required
         />
         <input
           type="text"
+          name="from_email"
           placeholder="Email"
           className="bg-slate-700 text-white w-11/12 md:w-4/5 p-2 h-12 rounded-md"
+          required
         />
         <textarea
           type="text"
+          name="message"
           placeholder="Message"
           className="bg-slate-700 text-white w-11/12 md:w-4/5 p-2 h-20 rounded-md"
+          required
         />
         <button
           type="submit"
@@ -42,6 +72,7 @@ const Footer = () => {
           Send
         </button>
       </form>
+        <ToastContainer />
     </footer>
   );
 };
