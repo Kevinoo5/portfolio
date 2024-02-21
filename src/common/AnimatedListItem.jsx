1bf766477b120c1d04128e23ecc24bfa3d9c32a2
Kevinoo5 <kevinnoormets@gmail.com>
2024-02-21 19:21:26 +0200

    Add project links project list and link them to images

--- src/common/AnimatedListItem.jsx
+++ src/common/AnimatedListItem.jsx
@@ -14,11 +14,13 @@ const AnimatedListItem = ({ project }) => {
     }`}
   >
     <div className="my-6 mx-8">
+      <a href={project.link}>
       <img
         className="rounded-3xl min-w-28 min-h-24 w-28 h-24 md:min-w-80 md:min-h-56 md:w-80 md:h-56"
         src={project.imageUrl}
         alt={project.title}
       />
+      </a>
     </div>
     <div className="my-4 mx-12 flex flex-col justify-center">
       <h2 className="text-white mb-8 text-2xl">{project.title}</h2>
--- src/data/projectList.jsx
+++ src/data/projectList.jsx
@@ -2,12 +2,14 @@ const projects = [
   {
     id: 1,
     title: "Aasta Tegija 2024 Competition",
+    link: "https://github.com/Kevinoo5/AastaTegija2024",
     imageUrl: "voco.png",
     description: "This was project was made for the Aasta Tegija 2024 competition. The competition was held on the 17-18th of January in 2024 at Tartu Vocational College, it was done in teams of two to three students who had to recreate a randomly chosen Figma prototype made by other students with code in about 12 hours. The prototype was made for the Baltic Games for Vocational Schools. The website was made using plain HTML and CSS"
   },
   {
     id: 2,
     title: "React Expense Tracker",
+    link: "https://github.com/Kevinoo5/expenses",
     imageUrl: "https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg",
     description: "This project was made to learn the basics of React. It is a simple expense tracker that allows the user to add and remove expenses."
   },
