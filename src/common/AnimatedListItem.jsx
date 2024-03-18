d6a60db63bc4f8b3090e509f229c2c1929a8fb6c
Kevinoo5 <kevinnoormets@gmail.com>
2024-03-18 22:20:28 +0200

    Add demo and repo buttons to projects along with new project

--- src/common/AnimatedListItem.jsx
+++ src/common/AnimatedListItem.jsx
@@ -14,13 +14,21 @@ const AnimatedListItem = ({ project }) => {
     }`}
   >
     <div className="my-6 mx-8">
-      <a href={project.link}>
       <img
-        className="rounded-3xl min-w-28 min-h-24 w-28 h-24 md:min-w-80 md:min-h-56 md:w-80 md:h-56"
+        className={`rounded-3xl min-w-28 min-h-24 w-28 h-24 md:min-w-80 md:min-h-56 md:w-80 md:h-56 ${project.extraClasses} `}
         src={project.imageUrl}
         alt={project.title}
       />
-      </a>
+      <div className="flex flew-row justify-center gap-14 mt-4">
+        <a href={project.link} target="_blank" rel="noreferrer">
+      <img src="github-mark-white.svg" alt="GitHub" className="w-8 h-8 mt-2" />
+        </a>
+        {project.liveDemo && (
+            <a href={project.liveDemo} target="_blank" rel="noreferrer">
+      <img src="globe.svg" alt="Live Demo" className="w-8 h-8 mt-2" />
+            </a>
+        )}
+      </div>
     </div>
     <div className="my-4 mx-12 flex flex-col justify-center">
       <h2 className="text-white mb-8 text-2xl">{project.title}</h2>
--- src/data/projectList.jsx
+++ src/data/projectList.jsx
@@ -4,21 +4,32 @@ const projects = [
     title: "Aasta Tegija 2024 Competition",
     link: "https://github.com/Kevinoo5/AastaTegija2024",
     imageUrl: "voco.png",
-    description: "This was project was made for the Aasta Tegija 2024 competition. The competition was held on the 17-18th of January in 2024 at Tartu Vocational College, it was done in teams of two to three students who had to recreate a randomly chosen Figma prototype made by other students with code in about 12 hours. The prototype was made for the Baltic Games for Vocational Schools. The website was made using plain HTML and CSS"
+    description: "This was project was made for the Aasta Tegija 2024 competition. The competition was held on the 17-18th of January in 2024 at Tartu Vocational College, it was done in teams of two to three students who had to recreate a randomly chosen Figma prototype made by other students with code in about 12 hours. The prototype was made for the Baltic Games for Vocational Schools. The website was made using plain HTML and CSS",
+    liveDemo: "https://aasta-tegija-2024.vercel.app/"
   },
   {
     id: 2,
     title: "React Expense Tracker",
     link: "https://github.com/Kevinoo5/expenses",
     imageUrl: "https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg",
-    description: "This project was made to learn the basics of React. It is a simple expense tracker that allows the user to add and remove expenses."
+    description: "This project was made to learn the basics of React. It is a simple expense tracker that allows the user to add and remove expenses.",
+    liveDemo: "https://expenses-hazel.vercel.app/"
   },
   {
     id: 3,
+    link: "https://github.com/Kevinoo5/portfolio",
     title: "My Portfolio Website",
     link: "https://github.com/Kevinoo5/portfolio",
     imageUrl: "portfolio.png",
     description: "This is my portfolio website. It was made using React and Tailwind CSS. It is a single page application that I created to showcase my skills and projects. It is also a great way to touch up on my React skills and learn about Tailwind CSS."
+  },
+  {
+    id: 4,
+    title: "Wurtle",
+    link: "https://github.com/Kevinoo5/Wurtle-projekt/tree/main",
+    imageUrl: "wordle.png",
+    extraClasses: "opacity-60",
+    description: "Wurtle is a simple word guessing game where the player tries to guess a hidden word of a specific length by entering words of the same length. It is a copy of the popular web game Wordle, but with a few extra features. The game interface is built using the Tkinter library in Python. The game randomly selects a word from a database and provides feedback to the player based on their guesses. I created this project with a classmate to learn about using databases in Python and to practice using the Tkinter library.",
   },
 ];
 
