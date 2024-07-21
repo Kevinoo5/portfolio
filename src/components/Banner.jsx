bdb859f42d6ecf249928024f9e6edf04f26676fd
Kevinoo5 <kevinnoormets@gmail.com>
2024-07-21 12:28:19 +0300

    Banner text changes to single column earlier for better responsiveness

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,7 +3,7 @@ import BackgroundBlob from "../common/BackgroundBlob.jsx";
 const Banner = () => {
   return (
     <div
-      className="relative overflow-hidden px-16 pt-36 lg:pt-24 pb-3 h-svh w-full grid grid-cols-1 sm:grid-cols-2 items-center gap-8"
+      className="relative overflow-hidden px-16 pt-36 lg:pt-24 pb-3 h-svh w-full grid grid-cols-1 md:grid-cols-2 items-center gap-8"
       id="home"
       style={{
         backgroundImage: "url('sunset.jpg')",
@@ -26,8 +26,8 @@ const Banner = () => {
         position={"top-48 left-1/4"}
         opacity={"opacity-20"}
       />
-      <div className="text-white pr-0 z-content sm:pr-24 w-full">
-        <h1 className="text-5xl font-bold lg:text-8xl py-3 sm:py-6">
+      <div className="text-white z-content w-full">
+        <h1 className="text-6xl font-bold lg:text-8xl py-3 sm:py-6">
           Welcome to my portfolio.
         </h1>
         <p className="mt-3 md:mt-6 w-full text-md sm:text-lg">
